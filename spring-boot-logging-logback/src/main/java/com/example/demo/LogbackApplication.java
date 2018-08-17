664cf76a0df186ea1988e2c4fe1ce4d7fa0b8f5c
zcy0521 <zcy19880521@sina.com>
2018-08-17 22:25:10 +0800

    logging

---
+++ spring-boot-logging-logback/src/main/java/com/example/demo/LogbackApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * doc: https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/
+ */
+@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
+public class LogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackApplication.class, args);
+    }
+
+}
